ad0c5a7f3bd602d875360a407ac7ba5ae74a41d6
Jake Brukhman <jbrukh@gmail.com>
2011-11-22 23:13:38 -0500

    First working copy.

--- bayesian.go
+++ bayesian.go
@@ -1,14 +1,56 @@
+// A Naive Bayesian Classifier
+// Jake Brukhman <jbrukh@gmail.com>
+
+// 
+// BAYESIAN CLASSIFICATION REFRESHER: suppose you have a set
+// of classes (e.g. categories) C := {C_1, ..., C_n}, and a
+// document D consisting of words D := {W_1, ..., W_k}.
+// We wish to ascertain the probability that the document
+// belongs to some class C_j, given some set of training data
+// associating documents and classes.
+//
+// By Bayes Theorem, we have that
+//
+//    P(C_j|D) = P(D|C_j)*P(C_j)/P(D).
+//
+// The LHS is the probability that the document belongs to class
+// C_j, given the document itself, and our program will calculate
+// this probability for each j and spit out the most likely class.
+// P(C_j) is referred to as the "prior" probability, or the
+// probability that a document belongs to C_j in general, without
+// seeing the document first. P(D|C_j) is the probability of seeing
+// such a document, given that it belongs to C_j. Here, by assuming
+// that words appear independently in documents (e.g. naive
+// assumption), we can estimate
+//
+//    P(D|C_j) ~= P(W_1|C_j)*...*P(W_k|C_j)
+//
+// where P(W_l|C_j) is the probability of seeing the given word
+// in a document of the given class. Finally, P(D) is merely a
+// scaling factor and is not particularly relevant, unless you want
+// to normalize the resulting scores and actually see
+// probabilities. In this case, note that
+//
+//    P(D) = SUM_j(P(D|C_j)*P(C_j)
+//
+// End of refresher.
+//
 package bayesian
 
-import "bytes"
-import "fmt"
-
+// defaultProb is the tiny non-zero probability that a word
+// we have not seen before appears in the class. 
 const defaultProb = 0.001
 
-// This type defines a set of classes
-// that the classifier will filter.
-// C = {C1, ..., Cn}
-type Class int
+// This type defines a set of classes that the classifier will
+// filter: C = {C_1, ..., C_n}. You should define your classes
+// as a set of constants, for example as follows:
+//
+//    const (
+//        Good Class = "good"
+//        Bad Class = "bad
+//    )
+//
+type Class string
 
 // Classifier implements the Naive Bayesian Classifier.
 type Classifier struct {
@@ -41,7 +83,7 @@ func (this *classData) getWordProb(word string) float32 {
     return float32(value)/float32(this.total)
 }
 
-// P(D|Cj)
+// P(D|C_j)
 // Note that words should not be empty
 func (this *classData) getWordsProb(words []string) (prob float32) {
     prob = 1
@@ -62,6 +104,8 @@ func New(classes []Class) (inst *Classifier) {
     return
 }
 
+// getPriors returns the prior probabilities for the
+// classes provided -- P(C_i).
 func (this *Classifier) getPriors() (priors []float32) {
     n := len(this.classes)
     priors = make([]float32, n, n)
@@ -79,6 +123,7 @@ func (this *Classifier) getPriors() (priors []float32) {
     return
 }
 
+// Learn will train the classifier on the provided data.
 func (this *Classifier) Learn(words []string, which Class) {
     data := this.datas[which]
     for _, word := range words {
@@ -92,7 +137,18 @@ func (this *Classifier) Learn(words []string, which Class) {
     }
 }
 
-func (this *Classifier) Score(words []string) (scores []float32) {
+// Score will produce an array of probabilities that correspond
+// to its opinion on the document in question, and whether it
+// belongs to the given class. The order of the probabilities
+// in the return values follows the order of the inital array
+// of Class objects parameterized to the New() function. If no
+// training data has been provided, this will return a 0 array.
+//
+// Additionally, this function will return the most likely Class,
+// as well whether this maximum is strict. If it is not strict,
+// this means that more than one class has the same maximum
+// probability.
+func (this *Classifier) Score(words []string) (scores []float32, likely Class, strict bool) {
     n := len(this.classes)
     scores = make([]float32, n, n)
     priors := this.getPriors()
@@ -103,18 +159,17 @@ func (this *Classifier) Score(words []string) (scores []float32) {
         scores[index] = score
         sum += score
     }
+    inx := 0
+    strict = true
     for i := 0; i < n; i++ {
         scores[i] /= sum
+        if scores[inx] < scores[i] {
+            inx = i
+            strict = true
+        } else if scores[inx] == scores[i] && i != 0 {
+            strict = false
+        }
     }
+    likely = this.classes[inx]
     return
 }
-
-func PrintArray(arr []float32) (str string) {
-    buffer := bytes.NewBufferString("")
-    fmt.Fprint(buffer, "[")
-    for _, item := range arr {
-        fmt.Fprint(buffer, item, ", ")
-    }
-    fmt.Fprint(buffer, "]")
-    return string(buffer.Bytes())
-} 
--- bayesian_test.go
+++ bayesian_test.go
@@ -3,33 +3,45 @@ package bayesian
 import "testing"
 import "fmt"
 
+const (
+    Good Class = "good"
+    Bad Class = "bad"
+)
+
+func Assert(t *testing.T, condition bool, args ...interface{}) {
+    if (!condition) {
+        t.Fatal(args)
+    }
+}
+
 func TestEmpty(t *testing.T) {
-    c := New([]Class{1,2,3})
+    c := New([]Class{"Good", "Bad", "Neutral"})
     priors := c.getPriors()
     for _, item := range priors {
-        if item != 0 {
-            t.Fail()
-        }
+        Assert(t, item == 0)
     }
 }
 
 func TestLearn(t *testing.T) {
-    const (
-        Good Class = 0
-        Bad Class = 1
-    )
     c := New([]Class{Good, Bad})
     c.Learn([]string{"tall", "handsome", "rich"}, Good)
     c.Learn([]string{"bald", "poor", "ugly"}, Bad)
     
-    score := c.Score([]string{"the", "tall", "man"})
+    score, likely, strict := c.Score([]string{"the", "tall", "man"})
     fmt.Printf("%v\n", score)
-    if score[0] <= score[1] {
-        t.Fail()
-    }
-    score = c.Score([]string{"the", "bad", "man"})
+    Assert(t, score[0]>score[1], "not good, round 1") // this is good
+    Assert(t, likely == Good, "not good, round 1")
+    Assert(t, strict == true, "not strict, round 1")
+     
+    score, likely, strict = c.Score([]string{"poor", "ugly", "girl"})
     fmt.Printf("%v\n", score)
-    if score[0] != score[1] {
-        t.Fail()
-    }
+    Assert(t, score[0]<score[1]) // this is bad
+    Assert(t, likely == Bad)
+    Assert(t, strict == true)
+    
+    score, likely, strict  = c.Score([]string{"the", "bad", "man"})
+    fmt.Printf("%v\n", score)
+    Assert(t, score[0]==score[1], "not the same") // same
+    Assert(t, likely == Good, "not good") // first one is picked
+    Assert(t, strict == false, "not strict")
 }
